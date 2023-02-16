d6cdfa7c1af1e5bbe407da2bfab87c3f8fe04ec0
Ladislav Hano <524934@fi.muni.cz>
2023-02-16 21:27:41 +0100

    Updated for dark theme

--- content.js
+++ content.js
@@ -1,20 +1,38 @@
-let brandejsMucitel = browser.runtime.getURL("assets/brandejsMucitel.jpg");
-let brandejsVino = browser.runtime.getURL("assets/brandejsWine.jpg");
-let brandejsMinecraft = browser.runtime.getURL("assets/brandejsMC.jpg");
-let brandejsMatrix = browser.runtime.getURL("assets/brandejsMatrix.jpg");
-let brandejsNeon = browser.runtime.getURL("assets/brandejsNeon.jpg");
+let photos = [browser.runtime.getURL("assets/brandejsWine.jpg"),
+                browser.runtime.getURL("assets/brandejsMatrix.jpg"),
+                browser.runtime.getURL("assets/brandejsNeon.jpg")];
 
-let photos = [brandejsMucitel, brandejsVino, brandejsMinecraft, brandejsMatrix, brandejsNeon];
+let other_css = `
+    ul.is-switch { float: left; }
+    #prepinace > ul > li > a { text-align: left; }
+    #app_menu.empty { background-color: transparent; }
+    .zivot_column { margin-top: 120px; }
+    .motiv-titulka .app_header_motiv_switch { 
+        float: none;
+        right: 0;
+        top: 0 !important; }
+    .ikony { 
+        float: left;`;
+if (document.body.classList.contains("motiv-tmavy")){
+    photos.concat(browser.runtime.getURL("assets/brandejsMucitel.png"), browser.runtime.getURL("assets/brandejsMC.png"));
+}
+else {
+    photos.concat(browser.runtime.getURL("assets/brandejsMC.jpg"), browser.runtime.getURL("assets/brandejsMucitel.jpg"));
+    other_css += `padding-left: 25%;
+                background-color: rgba(255, 255, 255, 0.7);
+                border-radius: 20px;`;
+}
+    other_css += "}";
 
 let rng = Math.floor(Math.random() * photos.length);
 
 let main_menu_css = `    
-    #content.motiv-titulka {
-        background-image: url('${photos[rng]}');
-        background-repeat: no-repeat;
-        background-position: 75% 0;
-        background-size: 550px auto;
-    }`
+#content.motiv-titulka {
+    background-image: url('${photos[rng]}');
+    background-repeat: no-repeat;
+    background-position: 75% 0;
+    background-size: 550px auto;
+}`;
 
 let app_css = `
     #app_header_image {
@@ -27,22 +45,7 @@ let app_css = `
     #app_header_image::after {
         background-image:  none;
     }
-    #app_menu.empty { background-color: transparent }`
-
-let other_css = `
-    ul.is-switch { float: left; }
-    #prepinace > ul > li > a { text-align: left; }
-    #app_menu.empty { background-color: transparent; }
-    .zivot_column { margin-top: 120px; }
-    .motiv-titulka .app_header_motiv_switch { 
-        float: none;
-        right: 0;
-        top: 0 !important; }
-    .ikony { 
-        float: left;
-        padding-left: 25%;
-        background-color: rgba(255, 255, 255, 0.7);
-        border-radius: 20px; }`
+    #app_menu.empty { background-color: transparent }`;
 
 let style = document.getElementsByTagName("style");
 if (style.length == 0) {
